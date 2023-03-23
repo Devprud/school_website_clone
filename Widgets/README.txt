23b732ed3d3d361ecfd6cecaa2ebcd91cc97c2b0
Devesh <7996devesh@gmail.com>
2023-03-23 19:33:59 +0530

    Add files via upload

---
+++ Widgets/README.txt
@@ -0,0 +1,3 @@
+Nothing special here in this folder.
+These are the parts i made in a seperate folder so that i won't get confused
+🙂
---
+++ js/expirement.js
@@ -0,0 +1,6 @@
+let name = document.getElementsByClassName('name')
+// let class = document
+// console.log(name.innerText)
+if ((name.innerText)==0) {
+    console.log('no text')
+}
